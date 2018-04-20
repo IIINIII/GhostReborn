317fa1743e846400ac243ed14056d2f8e1ee1542
IIINIII <ibrokhim.shokirov@gmail.com>
2018-04-20 14:09:47 +0300

    Initial commit

---
+++ src/main/java/com/ibrokhim/ghostreborn/GhostRebornApplication.java
@@ -0,0 +1,12 @@
+package com.ibrokhim.ghostreborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostRebornApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhostRebornApplication.class, args);
+    }
+}
